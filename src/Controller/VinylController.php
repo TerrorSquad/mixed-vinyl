2ca950c0116953e02387f847bd499dd745a532a1
Goran Ninkovic <g.ninkovic@youweagency.com>
2022-09-09 16:11:31 +0200

    feat: add wildcard route

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,13 +4,25 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class VinylController
 {
     #[Route('/')]
-    public function homepage(): Response {
+    public function homepage(): Response
+    {
         return new Response('Wohoo!');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        $title = 'All Genres';
+        if ($slug) {
+            $title = 'Genre ' . u(str_replace('-', ' ', $slug))->title(true);
+        }
+        return new Response($title);
     }
 }
