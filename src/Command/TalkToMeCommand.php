2650eb7d3c43c1c29a319ce1421942269ec78626
Goran Ninkovic <g.ninkovic@youweagency.com>
2022-10-09 18:34:43 +0200

    feat: implement app:talk-to-me command

--- src/Command/TalkToMeCommand.php
+++ src/Command/TalkToMeCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Service\MixRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -16,28 +17,34 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class TalkToMeCommand extends Command
 {
+    public function __construct(private MixRepository $mixRepository, string $name = null)
+    {
+        parent::__construct($name);
+    }
+
     protected function configure(): void
     {
         $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-        ;
+            ->addArgument('name', InputArgument::OPTIONAL, 'Your name')
+            ->addOption('yell', null, InputOption::VALUE_NONE, 'Shall I yell?');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
-
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        $name = $input->getArgument('name') ?: 'whoever you are';
+        $isYelling = $input->getOption('yell');
+        $msg = sprintf('Hello %s!', $name);
+        if ($isYelling) {
+            $msg = strtoupper($msg);
         }
+        $io->success($msg);
 
-        if ($input->getOption('option1')) {
-            // ...
+        if ($io->confirm('Do you want to see the mixes?')) {
+            $mixes = $this->mixRepository->findAll();
+            $mix = $mixes[array_rand($mixes)];
+            $io->note('I recommend the mix: ' . $mix['title']);
         }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
 
         return Command::SUCCESS;
     }
